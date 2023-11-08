42c5c284caf55d09fb88aa98dae464c433f3b83b
damon6291 <damon6291@gmail.com>
2023-11-08 14:27:47 -0500

    Updates

---
+++ precache-manifest.a2a9d024ff571733ad2a6a1516ada8a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a4e5b75959305ce3752b6b7ef96fb8",
+    "url": "/Portfolio-V2/index.html"
+  },
+  {
+    "revision": "a9940c9040949f74e13b",
+    "url": "/Portfolio-V2/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6c02d127e4c241dc14bd",
+    "url": "/Portfolio-V2/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "a9940c9040949f74e13b",
+    "url": "/Portfolio-V2/static/js/2.9056cd71.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio-V2/static/js/2.9056cd71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c02d127e4c241dc14bd",
+    "url": "/Portfolio-V2/static/js/main.2ae484d3.chunk.js"
+  },
+  {
+    "revision": "57b12c7e4d089d2ccaf3",
+    "url": "/Portfolio-V2/static/js/runtime-main.d611db1b.js"
+  }
+]);
